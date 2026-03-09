546725043972b12c6041380d382daa1bba8f67b9
priya kanani <priyakanani1234@gmail.com>
2026-03-09 09:46:16 +0530

    add the login page

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get("/", (req,res)=>{
+  res.send("Word Puzzle API Running 🚀");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
