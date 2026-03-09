3a14081a117d1eef814761a6b98274f46cee2816
priya kanani <priyakanani1234@gmail.com>
2026-03-09 08:31:21 +0530

    add the login page

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -1,5 +1,6 @@
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.registeruser = async (req,res) => {
   try {
@@ -25,6 +26,33 @@ exports.registeruser = async (req,res) => {
     });
   }
   catch (err) {
+    res.status(500).json({ message: err.message});
+  }
+}
+
+exports.loginUser = async (req,res) => {
+  try{
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+
+    if(!user){
+      return res.status(400).json({message : "User not found"});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return res.status(400).json({message : "Invalid Password"});
+    }
+
+    const token = jwt.sign(
+      {id: user._id,email : user.email},
+      process.env.JWT_SECRET,
+      { expiresIn: "10d"}
+    );
+
+    res.status(200).json({ message: "Login successful",token,user});
+  }
+  catch(err){
     res.status(500).json({ message: err.message});
   }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const jwtAuth = require("../middleware/authMiddleware");
 const register = require("../controller/usercontroller");
 
 router.post("/register", register.registeruser);
+router.post("/login", register.loginUser);
 
 module.exports = router;
